ffe1059addbab89b89d131182c4c6bac93b9f62b
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-08-13 23:00:24 +0530

    Refactor binary search implementations and add nextGreatestLetter function; remove test calls and add new tempCodeRunnerFile

--- binarySearch/implementation/index.js
+++ binarySearch/implementation/index.js
@@ -1,5 +1,5 @@
-// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-const arr = [99,95,94,80,10,9,8,7,3,2,1];
+// const arr = [1, 2, 3, 4, 5, 6, 9, 15, 25, 30];
+const arr = [99, 95, 94, 80, 10, 9, 8, 7, 3, 2, 1];
 
 function binarySearch(target) {
   let start = 0,
@@ -18,7 +18,7 @@ function binarySearch(target) {
   }
 }
 
-binarySearch(5);
+// binarySearch(5);
 
 //we dont know here weather out array is sorted or not
 
@@ -29,7 +29,6 @@ function agnosticBinarySearch(target) {
   let isAsc = arr[start] < arr[end] ? true : false;
 
   console.log(isAsc);
-  
 
   while (start <= end) {
     let mid = Math.floor(start + (end - start) / 2);
@@ -59,4 +58,65 @@ function agnosticBinarySearch(target) {
   return -1;
 }
 
-agnosticBinarySearch(2);
+// agnosticBinarySearch(2);
+
+// function ceilSearch(target) {
+//   let start = 0,
+//     end = arr.length - 1;
+
+//   if (arr[end] < target) {
+//     return -1;
+//   }
+
+//   while (start <= end) {
+//     let mid = Math.floor(start + (end - start) / 2);
+//     if (arr[mid] === target) {
+//       console.log(mid);
+//       return mid;
+//     } else if (arr[mid] > target) {
+//       end = mid - 1;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+
+//   return start; //soo close man use your brain stupid
+// }
+
+// function floorSearch(target) {
+//   let start = 0,
+//     end = arr.length - 1;
+//   if (arr[end] < target) {
+//     return -1;
+//   }
+//   while (start <= end) {
+//     let mid = Math.floor(start + (end - start) / 2);
+//     if (arr[mid] === target) {
+//       console.log(mid);
+//       return mid;
+//     } else if (arr[mid] < target) {
+//       end = mid - 1;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+
+//   return start;
+// }
+
+// console.log(ceilSearch(6));
+
+// console.log(floorSearch(6));
+
+var nextGreatestLetter = function (letters, target) {
+  let start = 0,
+    end = letters.length - 1;
+
+  if (target.charCodeAt(0) > letters[end].charCodeAt(0)) {
+   
+    
+    return letters[0];
+  }
+};
+
+nextGreatestLetter(["x", "x", "y", "y"], "z");
